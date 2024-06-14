1f188fefefc858335c344f18f1b6696e2efe105d
xyy <3426435900@qq.com>
2024-06-14 15:19:47 +0800

    mix : csv存json格式 的拼写错误

--- 作业4/csv存json格式.py
+++ 作业4/csv存json格式.py
@@ -13,7 +13,7 @@ fr.close()
 for i in range(1,len(list1)):
     # 使用zip()函数将第一个元素和当前元素对应的部分打包成一个元组，然后
     # 使用dict()函数将元组转换为 字典。将转换后的字典赋值给列表list1中当前的元素
-    list1[i] = dict(zip(list[0],list[i]))
+    list1[i] = dict(zip(list1[0],list1[i]))
 
 #     使用JSON模块的 dump()函数 将列表list1 中从索引1开始到最后的部分转换为 JSON格式，
 #     并将结果写入到文件fw中。参数indent=4 指定缩进为4个空格，

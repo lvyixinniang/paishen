93882f59dcfe65d259f46c71f7c38067e1100b90
xyy <3426435900@qq.com>
2024-06-16 10:58:50 +0800

    mix: 字符统计

--- 作业2/字符统计.py
+++ 作业2/字符统计.py
@@ -1,16 +1,20 @@
 import random
+
 txt = '''Ifthereisonlyoneargument,itmustbeadictionarymappingUnicode |ordinals(integers)orcharacterstoUnicodeordinals,stringsorNone. |Characterkeyswillbethenconvertedtoordinals. |Iftherearetwoarguments,theymustbestringsofequallength,and |intheresultingdictionary,eachcharacterinxwillbemappedtothe |characteratthesamepositioniny.Ifthereisathirdargument,it |mustbeastring,whosecharacterswillbemappedtoNoneintheresult.'''
-list1 = list(txt)
-x,m,n=map(int,input().split())
+List = list(txt)  # 将字符串转换为列表
 
-random.seed(x)
-random.shuffle(list1)
+x, m, n = map(int, input().split())  # 输入随机种子x和区间范围m和n
 
-Dict1 = {}
-for ch in list1:
-    Dict1[ch] = Dict1.get(ch,0)+1
+random.seed(x)  # 设置随机种子
+random.shuffle(List)  # 打乱列表顺序
 
-SortDict = sorted(Dict1.items(),key=lambda x:x[1],reverse=True)
+List1 = List[m:n + 1]  # 获取m到n之间的区间
 
-for i in range(0,5):
-    print("{}:{}".format(SortDict[i][0],SortDict[i][1]))
+Dict1 = {}  # 创建一个空字典用于统计字符出现次数
+for ch in List1:
+    Dict1[ch] = Dict1.get(ch, 0) + 1
+
+SortDict = sorted(Dict1.items(), key=lambda x: x[1], reverse=True)  # 按出现次数排序
+
+for i in range(0, 5):
+    print("{}:{}".format(SortDict[i][0], SortDict[i][1]))  # 输出前5个字符和次数
